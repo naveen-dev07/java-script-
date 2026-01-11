bfb43cf53a196049ec074805b90811c5e8912b3c
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-11 14:46:53 +0000

    comaparison in js

---
+++ js_journey/5_comparisons_.js
@@ -0,0 +1,29 @@
+// **********************compasirons*************************************
+
+
+
+// like <,>,= and  other there output in booleans 
+
+console.log(1>2); // false 
+console.log(2>1); // true 
+
+console.log(1>=2);  // false 
+
+console.log(1==2) // false 
+console.log(1!=2) // "not equal to " // true
+
+
+console.log("1" > 2); // automatic change into number and giving true:
+console.log(1 > "02"); // also this is true:
+
+console.log(null> 2)  // giving false 
+console.log(null== 2) // giving false 
+console.log(null>= 2) // giving true 
+
+// also undifined giving the same ans:
+
+//=== this used to strictly cheacking even there data types:
+
+console.log("1" === 2); // prints false but 20 line gives true
+
+console.log("2" === 2);  // this means it checks strictly even there data types so there output is false // tenision not we using more about these in if-else statment:
