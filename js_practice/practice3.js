7d2d05b6828c995e49f47a8cd6d9084ca91b334d
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-19 09:41:02 +0000

    a simple grade calculator

---
+++ js_practice/practice3.js
@@ -0,0 +1,19 @@
+// ++++++++++++++ A GRADE CALCULATOR ++++++++++++++
+let marks = 50
+if (marks == 90) {
+   console.log("A++");
+    
+}else if (marks == 80) {
+    console.log("A1");
+    
+    
+}else if (marks == 75) {
+    console.log("B");
+    
+    
+}else if (marks == 65) {
+    console.log("c");
+    
+}else {
+    console.log("you got failed")
+}
