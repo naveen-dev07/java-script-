b9a31d898e60ffb5935608a705c40ad837d0c661
Nav_6391 <singhnaveen9795@gmail.com>
2026-01-20 10:28:40 +0000

    another prectice max no. finder

---
+++ js_practice/practice4.js
@@ -0,0 +1,11 @@
+let myarr = [ 12,22,32,42,52]
+let max = 0
+for (let index = 0; index < myarr.length; index++) {
+   if (myarr[index] > max) {
+    max = myarr[index];
+
+    
+    
+   }
+}
+console.log("maximum number is", max);
